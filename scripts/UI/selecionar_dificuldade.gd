9057000839048863a868482e9c9b530c70c4c162
AlysonLimaOliveira <alyson_apps@hotmail.com>
2026-06-07 22:18:22 -0300

    feat:
    selecionador de fases, só falta integrar os níveis

--- scripts/UI/selecionar_dificuldade.gd
+++ scripts/UI/selecionar_dificuldade.gd
@@ -1,6 +1,23 @@
 extends Node2D
 
-@onready var label_atual: Label = $LabelAtual
+@onready var label_atual: Label = $Dificuldade
+@onready var garca_patrulheira = $GarcaMenu
+@onready var ponto_origem = $PontoOrigem
+@onready var botao_jogar = $BotaoJogar
+var resgatando: bool = false
+
+func _on_zona_de_queda_body_entered(body: Node2D) -> void:
+    print("Algo caiu na zona de queda: ", body.name) # Print para teste!
+    
+    if body.name == "player" and not resgatando:
+        resgatando = true
+        var pos_segura = ponto_origem.global_position
+        body.velocity = Vector2.ZERO
+        body.set_physics_process(false)
+        garca_patrulheira.iniciar_resgate(body, pos_segura)
+
+func _on_zona_de_queda_area_entered(area: Area2D) -> void:
+    pass
 
 func _ready() -> void:
     Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
--- scripts/UI/selecionar_fase.gd
+++ scripts/UI/selecionar_fase.gd
@@ -1,5 +1,24 @@
 extends Node2D
 
+@onready var garca_patrulheira = $GarcaMenu
+@onready var ponto_origem = $PontoOrigem
+@onready var botao_jogar = $BotaoJogar
+
+var resgatando: bool = false
+
+func _on_zona_de_queda_body_entered(body: Node2D) -> void:
+    print("Algo caiu na zona de queda: ", body.name) # Print para teste!
+    
+    if body.name == "player" and not resgatando:
+        resgatando = true
+        var pos_segura = ponto_origem.global_position
+        body.velocity = Vector2.ZERO
+        body.set_physics_process(false)
+        garca_patrulheira.iniciar_resgate(body, pos_segura)
+
+func _on_zona_de_queda_area_entered(area: Area2D) -> void:
+    pass
+
 const FASES = {
     1: "res://scenes/fases/fase1/fase1.tscn",
     2: "res://scenes/fases/fase2/fase2.tscn",
@@ -33,3 +52,29 @@ func _on_botao_voltar_pressed() -> void:
 
 func _on_botao_menu_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/UI/menu.tscn")
+
+# --- PLATAFORMAS DE FASES ---
+func _on_botao_fase_1_pressed() -> void:
+    selecionar_fase(1)
+
+func _on_botao_fase_2_pressed() -> void:
+    selecionar_fase(2)
+    
+func _on_botao_fase_3_pressed() -> void:
+    selecionar_fase(3)
+    
+func _on_botao_fase_4_pressed() -> void:
+    selecionar_fase(4)
+
+# --- PLATAFORMAS DE NÍVEIS (Dentro do painel de Níveis) ---
+func _on_botao_nivel_1_pressed() -> void:
+    selecionar_nivel(1)
+
+func _on_botao_nivel_2_pressed() -> void:
+    selecionar_nivel(2)
+    
+func _on_botao_nivel_3_pressed() -> void:
+    selecionar_nivel(3)
+
+func _on_botao_nivel_4_pressed() -> void:
+    selecionar_nivel(4)
