175449884cb60c82f7131ceaa76f70931f2a0388
Jeliell <jelielcorupa@gmail.com>
2026-06-15 21:18:57 -0300

    Cutscene_2
    Resto das cutscene

--- scripts/UI/menu.gd
+++ scripts/UI/menu.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+# Arraste a cutscene de abertura no Inspector
+@export var cutscene_inicio: CutsceneData = null
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_botao_jogar_pressed() -> void:
 	Configuracao.resetar_progresso()
-	get_tree().change_scene_to_file("res://scenes/fases/fase1/fase1.tscn")
+
+	# Se há cutscene de início, exibe antes da fase 1
+	if cutscene_inicio != null:
+		cutscene_inicio.cena_seguinte = "res://scenes/fases/fase1/fase1.tscn"
+		Configuracao.cutscene_atual = cutscene_inicio
+		get_tree().change_scene_to_file("res://scenes/UI/cutscene.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/fases/fase1/fase1.tscn")
 
 func _on_botao_selecionar_fase_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/selecionar_fase.tscn")
